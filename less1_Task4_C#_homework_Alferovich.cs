a33fbc8acb896815a1f7fcf080eae8573c32b179
AlexanderA <alexander.alferovich@gmail.com>
2023-01-11 13:16:04 +0400

    Final commit

--- less1_Task4_C#_homework_Alferovich.cs
+++ less1_Task4_C#_homework_Alferovich.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Пожалуйста, введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"Максимальное из трёх чисел: {max}");
